1b53ac40040bed8789d3cd92755030d3caef2445
MatiasRepetto <repetto.matias3@gmail.com>
2023-09-30 00:37:45 -0300

    adding recipes examples

---
+++ recipes/recipesPAO/scripts/4_World/classes/recipes/pluginrecipesmanagerbase.c
@@ -0,0 +1,20 @@
+modded class PluginRecipesManager 
+{
+	override void RegisterRecipies()
+    {
+        super.RegisterRecipies();				
+        RegisterRecipe(new metro_lamp_1);
+        RegisterRecipe(new BBP_Blueprint_1);
+        RegisterRecipe(new R9V_battery_charger_1);
+        RegisterRecipe(new RechargeableBattery9V_1);
+        RegisterRecipe(new LongLifeRechargeableBattery9V_1);
+        RegisterRecipe(new TLC_Bag_Kit_1);
+        RegisterRecipe(new TLC_Box_Kit_1);
+        RegisterRecipe(new TLC_5Box_Kit_1);
+        RegisterRecipe(new TLC_Rack_Kit_1);
+        RegisterRecipe(new TLC_Shelf_Kit_1);
+        RegisterRecipe(new TLC_ToolRack_Kit_1);
+    }
+}
+
+
